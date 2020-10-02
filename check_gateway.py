75321fe7657dbd9a8eb67fc7ceaf3eeac41ee265
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2020-10-02 10:52:29 +0530

    Add files via upload

---
+++ check_gateway.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import time
+
+os.system("clear")                                   
+os.system("arp -n")
+print
+time.sleep(1)
+print ("                  Author : Rahat Khan Tusar(RKT)")
+time.sleep(1)
+print ("                  Github : https:/github.com/r3k4t")
+print
